7d482a85ff473a36402ac3d2de7912b2b7ae08ee
LucaaDzs <lcs.sr1023@gmail.com>
2025-07-08 14:48:20 -0300

    Limpeza Geral e Revisão

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,43 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
     }
+};
+showMenu('nav-toggle', 'nav-menu');
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
 }
-showMenu('nav-toggle','nav-menu')
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const sections = document.querySelectorAll('section[id]');
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
-
-  sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-        
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }                                                    
-    })
-}
-window.addEventListener('scroll', scrollActive)
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
+        } else {
+            sectionsClass.classList.remove('active-link');
+        }
+    });
+};
+window.addEventListener('scroll', scrollActive);
+
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
@@ -50,13 +45,11 @@ const sr = ScrollReveal({
     delay: 100
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 300}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 300 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
 
-
-/*===== TOGGLE SKILLS IMAGE =====*/
 document.querySelectorAll('.toggle-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         const skill = btn.closest('.skill');
@@ -65,9 +58,9 @@ document.querySelectorAll('.toggle-btn').forEach(btn => {
 });
 
 document.querySelectorAll('.toggle-btn').forEach(btn => {
-  btn.addEventListener('click', function () {
-    const skillBox = btn.closest('.skill');
-    skillBox.classList.toggle('active');
-    skillBox.classList.toggle('active-bar');
-  });
+    btn.addEventListener('click', function () {
+        const skillBox = btn.closest('.skill');
+        skillBox.classList.toggle('active');
+        skillBox.classList.toggle('active-bar');
+    });
 });
